720cd257e4ee3449b56233b934a252b7913d4f6b
abi-v-j <abijoy611@gmail.com>
2024-06-18 02:33:56 -0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import axios from 'axios'
 import React, { useEffect } from 'react'
 
 const App = () => {
- const apiKey = 'f665ed3ec1602e6a94fcf6b4d4f391b0'
 
   const fetchMovie = async () => {
     const response = await axios.get(
       'https://api.themoviedb.org/3/discover/movie',
       {
         params: {
-          api_key: apiKey,
+          api_key: API_KEY,
           language: 'en-US',
           page: 1,
         },
