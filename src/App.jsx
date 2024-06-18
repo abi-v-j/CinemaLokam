6b81f9d23eb510cebda3fdc35b61ecc9e539fb28
abi-v-j <abijoy611@gmail.com>
2024-06-18 03:37:38 -0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
+const dotenv = require('dotenv');
+API_KEY = f665ed3ec1602e6a94fcf6b4d4f391b0
 
 const App = () => {
+  console.log(process.env.API_KEY);
 
   const fetchMovie = async () => {
     const response = await axios.get(
       'https://api.themoviedb.org/3/discover/movie',
       {
         params: {
-          api_key: API_KEY,
+          api_key: process.env.API_KEY,
           language: 'en-US',
           page: 1,
         },
